43991275263a580dcf9af60ea7bf37e4ff22e442
@Sergio007 <21sgomez@ibadia.cat>
2023-03-22 18:33:45 +0100

    Conexion a la base de datos con postgres

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/', Express.static('public'))
-app.use('/', Express.static(path.join(_dirname, 'public')))
-app.get('/', (req, res) => {
-  res.send('Hola, mundo!');
-  res.json([{"data": "Hello world"}])
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ProyectJS',
+  password: 'hola',
+  port: 5432,
+});
+
+app.get('/', express.static('public'))
+// app.use('/', Express.static(path.join(_dirname, 'public')))
+// app.get('/', (req, res) => {
+//   res.send('Hola, mundo!');
+//   res.json([{"data": "Hello world"}])
+// });
+
+app.get('/actor', (req, res) => {
+  pool.connect();
+  pool.query('SELECT * FROM actor', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
 });
 
 app.listen(3000, () => {
